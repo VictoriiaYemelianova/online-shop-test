a330d6b8341b6693ed588ebf4c105f99154d5a1d
Victoriia Yemelianova <emelyanovavikaa@gmail.com>
2020-04-06 16:33:49 +0300

    move to folder

--- src/app/Guard/is-admin/is-admin.guard.ts
+++ src/app/Guard/is-admin/is-admin.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { UserServiceService } from '../service/user-service.service';
+import { UserServiceService } from '../../service/user-service.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
