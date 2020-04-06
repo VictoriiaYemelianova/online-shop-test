6368a56c01364e9c648fa2558a0e6dd3826a670c
Victoriia Yemelianova <emelyanovavikaa@gmail.com>
2020-04-06 15:07:07 +0300

    add guard.ts

---
+++ src/app/Guard/is-admin.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { UserServiceService } from '../service/user-service.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsAdminGuard implements CanActivate {
+
+  constructor( private userService: UserServiceService, private router: Router ) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | boolean {
+    if (this.userService.logUser && this.userService.logUser.login === 'Admin') {
+      return true;
+    } else {
+      this.router.navigate(['']);
+    }
+  }
+}
