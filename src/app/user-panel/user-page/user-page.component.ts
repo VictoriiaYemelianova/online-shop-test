07b926203d01a699417dc9127b4fc2c279de2377
Victoriia Yemelianova <emelyanovavikaa@gmail.com>
2020-07-06 14:52:42 +0300

    changed styles

--- src/app/user-panel/user-page/user-page.component.ts
+++ src/app/user-panel/user-page/user-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { UserServiceService } from 'src/app/service/user-service.service';
 import { IProduct } from 'src/app/data-interface';
 
@@ -15,7 +15,10 @@ export class UserPageComponent implements OnInit, OnDestroy {
   private destroy: Subject<void> = new Subject<void>();
 
   public isAdmin = false;
+
   public faShoppingBasket = faShoppingCart;
+  public logoutIcon = faSignOutAlt;
+
   public userProductSize: number;
 
   constructor( private router: ActivatedRoute, private userService: UserServiceService ) { }
