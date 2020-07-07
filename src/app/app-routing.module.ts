007929737849f194da997d788cfb5fd9020988f3
Victoriia Yemelianova <emelyanovavikaa@gmail.com>
2020-07-07 14:40:01 +0300

    cahnget add-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { IsAdminGuard } from './Guard/is-admin/is-admin.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'enter-page', component: FirstPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'create-user', component: CreateAccountComponent },
+  { path: '', redirectTo: '/enter-page', pathMatch: 'full' },
+  { path: 'admin', loadChildren: () => import('./modules/admin.module').then(m => m.AdminModule),
+  canActivate: [IsAdminGuard] },
+  { path: 'user', loadChildren: () => import('./modules/user.module').then(m => m.UserModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { Routes, RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -28,31 +27,6 @@ import { ParamInterceptor } from './param.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SubcategoryComponent } from './subcategory/subcategory.component';
 
-const appRoutes: Routes = [
-  { path: 'enter-page', component: FirstPageComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'create-user', component: CreateAccountComponent },
-  { path: '', redirectTo: '/enter-page', pathMatch: 'full' },
-  { path: 'admin', loadChildren: () => import('./modules/admin.module').then(m => m.AdminModule),
-  canActivate: [IsAdminGuard] },
-  { path: 'user', loadChildren: () => import('./modules/user.module').then(m => m.UserModule) }
-  // { path: 'admin', component: AdminPageComponent, canActivate: [IsAdminGuard], children: [
-  //   { path: 'create-category', component:  CategoryFormComponent, data: {isAdmin: true} },
-  //   { path: 'categories', component: CategotiesComponent, data: {isAdmin: true} },
-  //   { path: 'categories/:name', component: ProductsComponent, data: {isAdmin: true} },
-  //   { path: 'categories/:name/create-product', component:  ProductFormComponent, data: {isAdmin: true} },
-  //   { path: 'categories/:name/:id', component: ProductsComponent, data: {isAdmin: true} }
-  // ] },
-  // { path: 'user', component: UserPageComponent, canActivate: [UserGuard], children: [
-  //   { path: 'categories', component: CategotiesComponent },
-  //   { path: 'categories/:name', component: ProductsComponent },
-  //   { path: 'categories/:name/:id', component: ProductsComponent },
-  //   { path: 'basket', component: ShopBasketComponent }
-  // ] }
-];
-
-// canActivate: [UserGuard],
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -76,7 +50,6 @@ const appRoutes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes),
     FontAwesomeModule
   ],
   providers: [
