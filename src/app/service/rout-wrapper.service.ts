3087402528f00fd1f184b02ee0a829a48a68f663
Victoriia Yemelianova <emelyanovavikaa@gmail.com>
2020-07-15 14:18:18 +0300

    added query params method

--- src/app/service/rout-wrapper.service.ts
+++ src/app/service/rout-wrapper.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { CategoryService } from './category.service';
 import { BehaviorSubject } from 'rxjs';
 
@@ -8,14 +8,17 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class RoutWrapperService {
   private routUrl: Array<string>;
+  private currentUrl: string;
   public breadcrumbs: BehaviorSubject<Array<string>> = new BehaviorSubject(null);
 
   constructor(
     private router: Router,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private route: ActivatedRoute
   ) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
+        this.currentUrl = event.url;
         this.routUrl = this.createArray(event.url);
         this.categoryService.checkRebootPage(this.routUrl[1], this.routUrl[2]);
         this.breadcrumbs.next(this.routUrl);
@@ -24,8 +27,20 @@ export class RoutWrapperService {
   }
 
   createArray(str) {
+    if (str.lastIndexOf('?') !== -1) {
+      str = str.substring(0, str.lastIndexOf('?'));
+    }
     const newArray = str.split('/');
     newArray.splice(0, 2);
     return newArray;
+  }
+
+  addQueryParams(param: object) {
+    this.router.navigate([],
+    {
+      relativeTo: this.route,
+      queryParams: param,
+      queryParamsHandling: 'merge'
+    });
   }
 }
