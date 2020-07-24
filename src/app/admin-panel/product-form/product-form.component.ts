351327300af27f6c9de073366b950ae6cb0b5421
Victoriia Yemelianova <emelyanovavikaa@gmail.com>
2020-07-24 16:01:08 +0300

    add new filds

--- src/app/admin-panel/product-form/product-form.component.ts
+++ src/app/admin-panel/product-form/product-form.component.ts
@@ -6,7 +6,8 @@ import { takeUntil } from 'rxjs/operators';
 import { Location } from '@angular/common';
 
 import { ProductService } from 'src/app/service/product.service';
-import { IServerModel } from 'src/app/data-interface';
+import { IServerModel, ICategory, ISubcategory } from 'src/app/data-interface';
+import { CategoryService } from 'src/app/service/category.service';
 
 @Component({
   selector: 'app-product-form',
@@ -17,21 +18,29 @@ export class ProductFormComponent implements OnInit, OnDestroy {
   private destroy: Subject<void> = new Subject<void>();
 
   public productForm: FormGroup;
-  public categoryName: string;
+  public categories: Array<ICategory>;
+  public subcategories: any;
   public infoMessage: string;
 
   constructor(
     private router: ActivatedRoute,
     private productService: ProductService,
+    private categoryService: CategoryService,
     private location: Location
   ) { }
 
   ngOnInit(): void {
     this.router.params
-    .pipe(takeUntil(this.destroy))
-    .subscribe(res => this.categoryName = res.name);
+    .pipe(takeUntil(this.destroy));
+
+    this.categoryService.fullCategories.subscribe(res => {
+      this.categories = res;
+      // this.subcategories = res[0].Categories;
+    });
 
     this.productForm = new FormGroup({
+      category: new FormControl('', Validators.required),
+      subcategory: new FormControl('', Validators.required),
       name: new FormControl('', Validators.required),
       imgUrl: new FormControl('', Validators.required),
       price: new FormControl('', Validators.required)
@@ -43,21 +52,21 @@ export class ProductFormComponent implements OnInit, OnDestroy {
     this.destroy.complete();
   }
 
-  onCancel() {
-    this.location.back();
-  }
-
   onSubmitNewProduct() {
     const formValue = this.productForm.value;
-    this.productService.create(formValue, this.categoryName)
-      .subscribe((res: IServerModel) => {
-        if (res.success) {
-          this.infoMessage = 'Created successfully!';
-          this.productForm.reset();
-        } else {
-          this.infoMessage = 'Error!';
-        }
-      });
+    // this.productService.create(formValue, this.categoryName)
+    //   .subscribe((res: IServerModel) => {
+    //     if (res.success) {
+    //       this.infoMessage = 'Created successfully!';
+    //       this.productForm.reset();
+    //     } else {
+    //       this.infoMessage = 'Error!';
+    //     }
+    //   });
+  }
+
+  onChooseCategory(category: ICategory) {
+    this.subcategories = category.Categories;
   }
 
 }
